4bfb5ae31d5c8bf696163915faa10de591593c56
moriokadronesquad <minato.morioka@gmail.com>
2026-04-06 21:12:12 +0900

    Fix routing, tab state, font, sync reload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1948805279" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4236894524" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
